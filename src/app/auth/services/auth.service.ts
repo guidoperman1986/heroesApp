6fb204ed3566201625bf686eafbc2bad539e80ea
Guido <38866056+guidoperman1986@users.noreply.github.com>
2021-08-12 10:46:22 -0300

    proteccion de rutas

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Auth } from '../interfaces/auth.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private baseUrl: string = environment.baseUrl;
+  private _auth: Auth | undefined;
+
+  constructor(private http: HttpClient) { }
+
+  get auth(): Auth{
+    return {...this._auth!};
+  }
+
+  login(){
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`)
+                .pipe(
+                  tap((auth)=>this._auth = auth),
+                  tap((auth)=>localStorage.setItem('id', auth.id)),
+                )
+  }
+
+  verificaAutenticacion(): Observable<boolean> {
+    if (!localStorage.getItem('id')){
+      return of(false);
+    }
+
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`)
+              .pipe(
+                map(auth=>{
+                  this._auth = auth;
+                  return true;
+                })
+              )
+  }
+
+}
--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Auth } from 'src/app/auth/interfaces/auth.interface';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   opened!: boolean;
-  constructor() { }
+  auth!: Auth;
+
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.auth = this.authService.auth;
+  }
+
+  logout(){
+    this.router.navigate(['./auth']);
   }
 
 }
